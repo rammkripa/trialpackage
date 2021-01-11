a6fb202c8d944e6f34590b9682d9ac6346a06bbb
rammkripa <ram.m.kripa@gmail.com>
2021-01-11 21:21:39 +0530

    num tokenz tested and docced

--- R/num_tokenz.R
+++ R/num_tokenz.R
@@ -1,3 +1,12 @@
+#' How many tokens are in the strings?
+#'
+#' @param s vector of strings
+#'
+#' @return a number
+#' @export
+#'
+#' @examples
+#' num_tokenz(c("a bad wolf", "stole my", "oranges"))
 num_tokenz <- function(s){
   t <- data.frame(s)
   t2 <- tidytext::unnest_tokens(tbl = t, input = "s", output = "tokenz")
