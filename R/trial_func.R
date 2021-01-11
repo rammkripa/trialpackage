8ee23cac8f46be8aaaa0d71cd047bbaa5c65367a
rammkripa <ram.m.kripa@gmail.com>
2021-01-11 20:50:57 +0530

    third commit

--- R/trial_func.R
+++ R/trial_func.R
@@ -1,3 +1,14 @@
+#' Detect h in strings
+#'
+#' Return boolean vector that tells whether strings contain h
+#'
+#' @param a vector of strings
+#'
+#' @return boolean vector
+#' @export
+#'
+#' @examples
+#' trial_func(c("hi","bye","jello holiday"))
 trial_func <- function(a) {
   stringr::str_detect(a, 'h')
 }
